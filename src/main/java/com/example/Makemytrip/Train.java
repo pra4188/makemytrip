0ca03c338db45e14a4e6147ab6748710f53fbe6c
Prathamesh <pra4188@gmail.com>
2023-10-18 23:46:11 +0530

    Adding a Train  module

---
+++ src/main/java/com/example/Makemytrip/Train.java
@@ -0,0 +1,9 @@
+package com.example.Makemytrip;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Train {
+    @GetMapping("/Train")
+public String getData() {return "Welcome to Goibibo .Please book your Train Ticket at 40% Discount" ;}
+}
+
