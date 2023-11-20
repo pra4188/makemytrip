fed31e4b434fdd9c5c605aedcf3b482cc5958906
Prathamesh <pra4188@gmail.com>
2023-11-21 00:28:12 +0530

    Adding Jenkings file

--- src/main/java/com/example/Makemytrip/Train.java
+++ src/main/java/com/example/Makemytrip/Train.java
@@ -2,6 +2,7 @@ package com.example.Makemytrip;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+
 public class Train {
     @GetMapping("/Train")
 public String getData() {return "Welcome to Goibibo .Please book your Train Ticket at 40% Discount" ;}
